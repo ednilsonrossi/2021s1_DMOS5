dce0c8ba70647fb6972fe46eb0e3fa0d38b0189f
Ednilson Rossi <ednilsonrossi@gmail.com>
2021-05-03 19:22:37 -0300

    Exemplo de entrada decimal com sinal

---
+++ Entrada_Numerica/app/src/main/java/br/edu/ifsp/arq/domos5_2021/entrada_numerica/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.arq.domos5_2021.entrada_numerica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
