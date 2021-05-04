1a457ab1e9886d26053b07b5ada9f0ced1907ee3
Ednilson Rossi <ednilsonrossi@gmail.com>
2021-05-04 13:42:46 -0300

    Projeto App Medias_DMO vazio

---
+++ Medias_DMO/app/src/main/java/br/edu/ifsp/arq/domos5_2021/medias_dmo/MainActivity.java
@@ -0,0 +1,14 @@
+it lopackage br.edu.ifsp.arq.domos5_2021.medias_dmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
