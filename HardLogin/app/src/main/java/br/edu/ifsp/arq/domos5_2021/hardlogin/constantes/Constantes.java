31a5ceb90a6e33659329bd734f90e82789d04685
Ednilson Rossi <ednilsonrossi@gmail.com>
2021-05-05 19:30:41 -0300

    Armazena os dados do último login, caso o usuário marque a opção

--- HardLogin/app/src/main/java/br/edu/ifsp/arq/domos5_2021/hardlogin/constantes/Constantes.java
+++ HardLogin/app/src/main/java/br/edu/ifsp/arq/domos5_2021/hardlogin/constantes/Constantes.java
@@ -1,8 +1,9 @@
 package br.edu.ifsp.arq.domos5_2021.hardlogin.constantes;
 
 public class Constantes {
-    public static final String USER = "ednilsonrossi@ifsp.edu.br";
-    public static final int PASSWORD = 12345;
+//    public static final String USER = "ednilsonrossi@ifsp.edu.br";
+//    public static final int PASSWORD = 12345;
     public static final String KEY_USERNAME = "username";
     public static final String KEY_PASSWORD = "passwd";
+    public static final String KEY_SAVE_PREPERENCES = "lembrar";
 }
--- HardLogin/app/src/main/java/br/edu/ifsp/arq/domos5_2021/hardlogin/view/MainActivity.java
+++ HardLogin/app/src/main/java/br/edu/ifsp/arq/domos5_2021/hardlogin/view/MainActivity.java
@@ -3,9 +3,11 @@ package br.edu.ifsp.arq.domos5_2021.hardlogin.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,6 +20,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText mUserEditText;
     private EditText mPasswordEditText;
     private Button mLoginButton;
+    private CheckBox mSaveLoginCheckBox;
+
+    //Referências para objetos de preferências do usuário
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +34,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mUserEditText = findViewById(R.id.edit_user);
         mPasswordEditText = findViewById(R.id.edit_password);
         mLoginButton = findViewById(R.id.button_login);
+        mSaveLoginCheckBox = findViewById(R.id.check_save_data);
 
         //Configurando listener
         mLoginButton.setOnClickListener(this);
+
+        //Instanciar as predferências de modo privado, ou seja, somente acessível ao app;
+        mSharedPreferences = this.getPreferences(MODE_PRIVATE);
+
+        checkPreferences();
     }
+
+
 
     @Override
     public void onClick(View view) {
@@ -48,6 +62,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } catch (NumberFormatException ex){
             password = 0;
         }
+        //Grava as preferências do usuário
+        savePreferences(username, password);
 
         //Criar um pacote/embrulho de dados (Bundle) para enviar para nova Acvity
         Bundle embrulho = new Bundle();
@@ -59,5 +75,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Incluir o embrulho na intenção
         intencao.putExtras(embrulho);
         startActivity(intencao);
+    }
+
+    private void checkPreferences(){
+        String usuario = mSharedPreferences.getString(Constantes.KEY_USERNAME, "");
+        int senha = mSharedPreferences.getInt(Constantes.KEY_PASSWORD, -1);
+        boolean lembrar = mSharedPreferences.getBoolean(Constantes.KEY_SAVE_PREPERENCES, false);
+        if(lembrar){
+            mUserEditText.setText(usuario);
+            mPasswordEditText.setText(String.format("%d", senha));
+            mSaveLoginCheckBox.setChecked(true);
+        }
+    }
+
+    private void savePreferences(String username, int password){
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        if(mSaveLoginCheckBox.isChecked()){
+            editor.putString(Constantes.KEY_USERNAME, username)
+                    .putInt(Constantes.KEY_PASSWORD, password)
+                    .putBoolean(Constantes.KEY_SAVE_PREPERENCES, true);
+        }else{
+            editor.putString(Constantes.KEY_USERNAME, "")
+                    .putInt(Constantes.KEY_PASSWORD, -1)
+                    .putBoolean(Constantes.KEY_SAVE_PREPERENCES, false);
+        }
+        editor.commit();
     }
 }
