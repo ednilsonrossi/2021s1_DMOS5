d2c45b55e07d7e2f0da46f423c21229ced8b09cd
Ednilson Rossi <ednilsonrossi@gmail.com>
2021-05-15 15:48:49 -0300

    Meu Pocket: Adiciona novos sites e edita os sites já cadastrados.

--- MeuPocket/app/src/main/java/br/edu/ifsp/arq/domos5_2021/meupocket/dao/SiteDAO.java
+++ MeuPocket/app/src/main/java/br/edu/ifsp/arq/domos5_2021/meupocket/dao/SiteDAO.java
@@ -2,6 +2,9 @@ package br.edu.ifsp.arq.domos5_2021.meupocket.dao;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import br.edu.ifsp.arq.domos5_2021.meupocket.model.Site;
 
@@ -31,5 +34,24 @@ public class SiteDAO {
 
     public List<Site> getSites() {
         return siteList;
+    }
+
+    public void addSite(Site site){
+        if(site != null){
+            siteList.add(site);
+        }
+    }
+
+    public Site find(int i){
+        return siteList.get(i);
+    }
+
+    public Site find(String title){
+        for(Site s : siteList){
+            if(s.getTitle().equals(title)){
+                return s;
+            }
+        }
+        return null;
     }
 }
--- MeuPocket/app/src/main/java/br/edu/ifsp/arq/domos5_2021/meupocket/view/MainActivity.java
+++ MeuPocket/app/src/main/java/br/edu/ifsp/arq/domos5_2021/meupocket/view/MainActivity.java
@@ -1,18 +1,20 @@
 package br.edu.ifsp.arq.domos5_2021.meupocket.view;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 import br.edu.ifsp.arq.domos5_2021.meupocket.R;
+import br.edu.ifsp.arq.domos5_2021.meupocket.constantes.Constantes;
+import br.edu.ifsp.arq.domos5_2021.meupocket.controller.SiteController;
 import br.edu.ifsp.arq.domos5_2021.meupocket.dao.SiteDAO;
 import br.edu.ifsp.arq.domos5_2021.meupocket.model.Site;
 
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Site> mSites;
     private ListView mListView;
+    private FloatingActionButton mActionButton;
     private ArrayAdapter<Site> mSiteArrayAdapter;
 
     @Override
@@ -27,15 +30,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSites = SiteDAO.getInstance().getSites();
+        mSites = SiteController.allSites();
         mSiteArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mSites);
 
         mListView = findViewById(R.id.list_sites);
         mListView.setAdapter(mSiteArrayAdapter);
-        mListView.setOnItemClickListener((adapterView, view, i, l) -> Toast.makeText(
-                getApplicationContext(),
-                mSites.get(i).getTitle() + " | " + mSites.get(i).getUrl(),
-                Toast.LENGTH_SHORT).show()
-        );
+        mListView.setOnItemClickListener((adapterView, view, i, l) -> updateSite(i));
+
+        mActionButton = findViewById(R.id.fab_add_site);
+        mActionButton.setOnClickListener(v -> newSite());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case Constantes.REQUEST_CODE_NEW_SITE:
+                    SiteController.addSite(
+                            data.getStringExtra(Constantes.KEY_TITLE),
+                            data.getStringExtra(Constantes.KEY_URL)
+                    );
+                    break;
+                case Constantes.REQUEST_CODE_UPDATE_SITE:
+                    String o = data.getStringExtra(Constantes.KEY_OLD_TITLE);
+                    String t = data.getStringExtra(Constantes.KEY_TITLE);
+                    String u = data.getStringExtra(Constantes.KEY_URL);
+                    SiteController.updateSite(o, t, u);
+                    break;
+            }
+            updateAdapter();
+        }
+    }
+
+    private void newSite() {
+        Intent intent = new Intent(this, SiteActivity.class);
+        startActivityForResult(intent, Constantes.REQUEST_CODE_NEW_SITE);
+    }
+
+    private void updateSite(int position) {
+        Bundle bundle = new Bundle();
+        bundle.putString(Constantes.KEY_TITLE, mSites.get(position).getTitle());
+        bundle.putString(Constantes.KEY_URL, mSites.get(position).getUrl());
+
+        Intent intent = new Intent(this, SiteActivity.class);
+        intent.putExtras(bundle);
+        startActivityForResult(intent, Constantes.REQUEST_CODE_UPDATE_SITE);
+    }
+
+    private void updateAdapter(){
+        mSiteArrayAdapter.notifyDataSetChanged();
     }
 }
